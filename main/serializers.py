9b12941b161b750c1d5e4d0fe2b56c67645d4eac
angelina <angln.pnn@gmail.com>
2024-04-27 01:52:41 +0700

    Создан сериализатор для модели Query

---
+++ main/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from main.models import Query
+
+
+class QuerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Query
+        fields = [
+            'number',
+            'latitude',
+            'longitude']
