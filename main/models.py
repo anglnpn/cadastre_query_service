6db601ab21d6e7af524143febe48d38720bbbaf4
angelina <angln.pnn@gmail.com>
2024-04-27 01:15:38 +0700

    Добавлена модель для запроса по кадастровому номеру.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Query(models.Model):
+    """
+    Модель для сохранения данных
+    запроса по кадастровому номеру
+     в БД.
+    """
+    number = models.CharField(
+        max_length=50,
+        verbose_name='кадастровый номер')
+    latitude = models.FloatField(
+        verbose_name='широта')
+    longitude = models.FloatField(
+        verbose_name='долгота')
+    date_query = models.DateField(
+        auto_now=True,
+        verbose_name='дата запроса')
+    server_response = models.BooleanField(
+        verbose_name='ответ сервера',
+        null=True, blank=True)
